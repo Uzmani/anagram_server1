e2ff0f174e2009d82ceb95b1b81a2a5814026d9d
Apprentice <apprentice@dbc15.local>
2013-08-05 18:18:14 -0700

    add a few more routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,7 +1,15 @@
+get '/' do
+  erb :index
+end
+
 get '/:word' do
   # Look in app/views/index.erb
   @word = params[:word]
   @anagrams = Word.new(word: @word).anagrams
 
   erb :index
+end
+
+post '/' do
+  redirect "/#{params[:word]}"
 end
